e7294d9000db69f8e49a05723321c3c37bbe613d
Martin Jenkins <martin@gyromancy.com.au>
2017-03-05 10:00:17 +0000

    First runbook

---
+++ Runbook/Config-WebServer.ps1
@@ -0,0 +1,24 @@
+﻿Import-DscResource –ModuleName ’PSDesiredStateConfiguration’
+
+configuration TestConfig
+{
+    Node WebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+
+        }
+    }
+
+    Node NotWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
+        }
+    }
+}
